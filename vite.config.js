6e4556586d503d7baaf0fc2e589ae99d4377fcc1
tiwariudit0310@gmail.com <tiwariudit0310@gmail.com>
2025-06-07 16:44:51 +0530

    Deploye

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Recipe-project-React/',
   plugins: [react(), tailwindcss()],
 
   server: {
